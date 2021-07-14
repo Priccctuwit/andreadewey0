e6f5953d71af855447d8d5ed2942cbac58b0d89c
Priccctuwit <jiaanlanju0@126.com>
2021-07-14 20:16:03 +0800

    Upload miniView.js

--- miniView.js
+++ miniView.js
@@ -1,1 +1,126 @@
-var miniViewObject={selector:null,zoom:0.1,viewZoom:1,refresh:function(a){this.getParams(a);var b=this.selector,p=["webkit","moz","ms","o"],oString="left top";if(a&&a.viewZoom)this.viewZoom=a.viewZoom;var c=$(b).clone().removeAttr("style"),s="scale("+this.zoom+")",parentDiv=$(b).parent();for(var i=0;i<p.length;i++){c[0].style[p[i]+"Transform"]=s;c[0].style[p[i]+"TransformOrigin"]=oString}c[0].style["transform"]=s;c[0].style["transformOrigin"]=oString;$("#miniview "+b+"Mini").remove();$("#miniview").append(c.attr("id",$(b).attr("id")+"Mini").removeAttr("data-bind"));$("#minimap, #selectWin, #miniview").css({width:$(b).width()*this.zoom,height:$(b).height()*this.zoom});$(".selectionWin").css({width:parentDiv.width()*this.zoom/this.viewZoom,height:parentDiv.height()*this.zoom/this.viewZoom});var d=parseInt($(b).css("left")),selTop=parseInt($(b).css("top"));$(".selectionWin").css({left:-d*this.zoom/this.viewZoom,top:-selTop*this.zoom/this.viewZoom})},getParams:function(a){if(!a)return false;if(a.selector)this.selector=a.selector;if(a.zoom)this.zoom=a.zoom;if(a.viewZoom)this.viewZoom=a.viewZoom}};var setViewZoom=function(a,b,c,d){var e=a.delegateTarget,mousePosX=a.clientX-e.offsetLeft,mousePosY=a.clientY-e.offsetTop,origZoom=miniViewObject.viewZoom,zoom,elCurrLeft=parseInt($(e).css("left")),elCurrTop=parseInt($(e).css("top"));if(b>0&&origZoom<1.5)zoom=origZoom+0.05;else if(b<0&&origZoom>0.5)zoom=origZoom-0.05;else return false;var p=["webkit","moz","ms","o"],s="scale("+zoom+")",oString="left top";for(var i=0;i<p.length;i++){e.style[p[i]+"Transform"]=s;e.style[p[i]+"TransformOrigin"]=oString}e.style["transform"]=s;e.style["transformOrigin"]=oString;var f=mousePosX*(1-zoom/origZoom),TopDiff=mousePosY*(1-zoom/origZoom);$(e).css("left",elCurrLeft+f);$(e).css("top",elCurrTop+TopDiff);miniViewObject.refresh({viewZoom:zoom});return false};window.MiniView={init:function(e){miniViewObject.getParams(e);var f=e.selector;var g=$(f).parent();g.after('<div id="minimap">'+'<div id="selectWin">'+'<div class="selectionWin"></div>'+'</div>'+'<div id="miniview"></div>'+'</div>');$(".selectionWin").draggable({containment:'parent',drag:function(){var a=parseInt($(".selectionWin").css("left")),selectionTop=parseInt($(".selectionWin").css("top"));$(miniViewObject.selector).css({left:-a/(miniViewObject.zoom/miniViewObject.viewZoom),top:-selectionTop/(miniViewObject.zoom/miniViewObject.viewZoom)})}});$(f).mousewheel(function(a,b,c,d){setViewZoom(a,b,c,d);return false});miniViewObject.refresh();return miniViewObject}}
+var miniViewObject = {
+	selector: null,
+	zoom: 0.1,
+	viewZoom: 1,
+	refresh: function(config) {
+		this.getParams(config);
+		var selector = this.selector,
+			p = ["webkit", "moz", "ms", "o"],
+			oString = "left top";
+			
+		if (config && config.viewZoom)
+			this.viewZoom = config.viewZoom;
+		
+		var el = $(selector).clone().removeAttr("style"),
+			s = "scale(" + this.zoom + ")",
+			parentDiv = $(selector).parent();
+			
+		for (var i = 0; i < p.length; i++) {
+			el[0].style[p[i] + "Transform"] = s;
+			el[0].style[p[i] + "TransformOrigin"] = oString
+		}
+		
+		el[0].style["transform"] = s;
+		el[0].style["transformOrigin"] = oString;
+		
+		$("#miniview " + selector + "Mini").remove();
+		$("#miniview").append(el.attr("id", $(selector).attr("id") + "Mini").removeAttr("data-bind"));
+		
+		//设置小地图大小
+		$("#minimap, #selectWin, #miniview").css({
+			width: $(selector).width() * this.zoom,
+			height: $(selector).height() * this.zoom
+		});
+		
+		//设置选择框大小
+		$(".selectionWin").css({
+			width: parentDiv.width() * this.zoom / this.viewZoom,
+			height: parentDiv.height() * this.zoom / this.viewZoom
+		});
+		
+		//设置选择框位置
+		var selLeft = parseInt($(selector).css("left")),
+			selTop = parseInt($(selector).css("top"));
+		$(".selectionWin").css({
+			left: -selLeft * this.zoom / this.viewZoom,
+			top: -selTop * this.zoom / this.viewZoom
+		})
+	},
+	getParams: function(config) {
+		if (!config) return false;
+		if (config.selector) this.selector = config.selector;
+		if (config.zoom) this.zoom = config.zoom;
+		if (config.viewZoom) this.viewZoom = config.viewZoom
+	}
+};
+var setViewZoom = function(event, delta, deltaX, deltaY) {
+		var el = event.delegateTarget,
+			mousePosX = event.clientX - el.offsetLeft,
+			mousePosY = event.clientY - el.offsetTop,
+			origZoom = miniViewObject.viewZoom,
+			zoom, elCurrLeft = parseInt($(el).css("left")),
+			elCurrTop = parseInt($(el).css("top"));
+			
+		if (delta > 0 && origZoom < 1.5)
+			zoom = origZoom + 0.05;
+		else if (delta < 0 && origZoom > 0.5)
+			zoom = origZoom - 0.05;
+		else
+			return false;
+		
+		var p = ["webkit", "moz", "ms", "o"],
+			s = "scale(" + zoom + ")",
+			oString = "left top";
+			
+		for (var i = 0; i < p.length; i++) {
+			el.style[p[i] + "Transform"] = s;
+			el.style[p[i] + "TransformOrigin"] = oString
+		}
+		
+		el.style["transform"] = s;
+		el.style["transformOrigin"] = oString;
+		
+		var leftDiff = mousePosX * (1 - zoom / origZoom),
+			TopDiff = mousePosY * (1 - zoom / origZoom);
+		$(el).css("left", elCurrLeft + leftDiff);
+		$(el).css("top", elCurrTop + TopDiff);
+		
+		miniViewObject.refresh({
+			viewZoom: zoom
+		});
+		
+		return false
+	};
+window.MiniView = {
+	init: function(config) {
+		miniViewObject.getParams(config);
+		var selector = config.selector;
+		var parentDiv = $(selector).parent();
+		parentDiv.after('<div id="minimap">'
+							+ '<div id="selectWin">' 
+								+ '<div class="selectionWin"></div>'
+							+ '</div>'
+							+ '<div id="miniview"></div>'
+						+ '</div>');
+		//拖拽效果				
+		$(".selectionWin").draggable({
+			containment: 'parent',
+			drag: function() {
+				var selectionLeft = parseInt($(".selectionWin").css("left")),
+					selectionTop = parseInt($(".selectionWin").css("top"));
+				$(miniViewObject.selector).css({
+					left: -selectionLeft / (miniViewObject.zoom / miniViewObject.viewZoom),
+					top: -selectionTop / (miniViewObject.zoom / miniViewObject.viewZoom)
+				})
+			}
+		});
+		
+		//滑轮滚动缩放
+		$(selector).mousewheel(function(event, delta, deltaX, deltaY) {
+			setViewZoom(event, delta, deltaX, deltaY);
+			return false
+		});
+		miniViewObject.refresh();
+		return miniViewObject
+	}
+}
